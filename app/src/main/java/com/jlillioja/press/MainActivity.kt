f8ee4caca3c3dee7ed6fc6bc7ca4ea4a39fd2fb5
root <jacob@jlillioja.com>
2017-03-31 20:40:28 -0500

    Added Anko

--- app/src/main/java/com/jlillioja/press/MainActivity.kt
+++ app/src/main/java/com/jlillioja/press/MainActivity.kt
@@ -7,29 +7,27 @@ import android.widget.Toast
 import android.widget.Toast.LENGTH_SHORT
 import com.jlillioja.press.database.DatabaseManager
 import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.*
 import org.robolectric.annotation.Implements
 import javax.inject.Inject
 
 @Implements
-open class MainActivity : Activity(), HistoryFragment.OnFragmentInteractionListener {
+open class MainActivity : Activity() {
 
     @Inject
     open lateinit var databaseManager: DatabaseManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
         PressApplication.graph.inject(this)
-        submit.setOnClickListener { saveCurrent() }
 
-        setUpHistoryFragment()
-    }
-
-    private fun setUpHistoryFragment() {
-        fragmentManager.beginTransaction()
-                .add(R.id.history_fragment, HistoryFragment.newInstance())
-                .commit()
-
+        verticalLayout {
+            button {
+                onClick {
+                    saveCurrent()
+                }
+            }
+        }
     }
 
     fun saveCurrent() {
@@ -47,10 +45,6 @@ open class MainActivity : Activity(), HistoryFragment.OnFragmentInteractionListe
                     reps = reps.toInt(),
                     weight = weight.toInt())
         }
-    }
-
-    override fun onFragmentInteraction(uri: Uri) {
-        throw UnsupportedOperationException("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 }
 
